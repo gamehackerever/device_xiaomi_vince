4774a30147d9813e2132d7b10b0a1f75e0c068a7
Julian Fischer <myst3rion9@gmail.com>
2021-01-12 22:57:15 +0100

    vince: power-libperfmgr: Add DT2W based on powerhints
    Co-authored-by: Jesse Chan <jc@lineageos.org>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I11890470f7e11ed7a256851246272a1f11eaf0bc
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: gamehackerever <gamehackerever@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -155,7 +155,6 @@ BOARD_VENDORIMAGE_PARTITION_SIZE := 872415232
 TARGET_PER_MGR_ENABLED := true
 
 # Power
-TARGET_TAP_TO_WAKE_NODE := "/sys/devices/platform/soc/78b7000.i2c/i2c-3/3-0020/input/input2/wake_gesture"
 TARGET_USES_INTERACTION_BOOST := true
 
 # Qualcomm
