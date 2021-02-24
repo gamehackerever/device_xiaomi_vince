6194f25dc4be69e785df66a887e2de1e4350115a
gamehackerever <gamehackerever@gmail.com>
2021-02-24 04:25:22 +0000

    vince: Build sensorservice

--- havoc_vince.mk
+++ havoc_vince.mk
@@ -40,3 +40,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "xiaomi/vince/vince:8.1.0/OPM1.171019.019/V11.0.2.0.OEGMIXM:user/release-keys"
+
+PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0
