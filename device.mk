88b4a5abd8184d2690d42926369c8d9bcb0e56f0
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-03-21 22:10:21 +0100

    vince: Build camera HAL from source

--- device.mk
+++ device.mk
@@ -411,7 +411,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
-    
+
 # VNDK
 PRODUCT_PACKAGES += \
     libstdc++.vendor
