6c872f256c44aeb207f44e3dad5d3aaa26c1674b
Kingsman44 <shivan.0972@gmail.com>
2021-07-03 13:35:28 +0530

    vince: Add QTI Performace Oats in Boots Jars and pin it
    Signed-off-by: Kingsman44 <shivan.0972@gmail.com>

--- device.mk
+++ device.mk
@@ -351,6 +351,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libjson
 
+#QTI performance
+PRODUCT_BOOT_JARS += \
+    QPerformance \
+    UxPerformance
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
