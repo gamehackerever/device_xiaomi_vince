3728410d454365b1f7c20d3a108afb09c0bd2cf8
gamehackerever <gamehackerever@gmail.com>
2021-07-15 12:32:30 +0530

    vince: Arrow-ify
    Signed-off-by: gamehackerever <gamehackerever@gmail.com>

--- arrow_vince.mk
+++ arrow_vince.mk
@@ -27,12 +27,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 # Inherit from vince device
 $(call inherit-product, device/xiaomi/vince/device.mk)
 
-# Inherit some common Aosp stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common ArrowOS stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vince
-PRODUCT_NAME := aosp_vince
+PRODUCT_NAME := arrow_vince
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5 Plus
 PRODUCT_MANUFACTURER := Xiaomi
