edb841226737ec5e0c027194250669165a374e51
Sahil Ahmed <73250691+sahilhv@users.noreply.github.com>
2021-07-22 22:57:58 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_vince.mk
+    $(LOCAL_DIR)/fluid_vince.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_vince-user \
-    arrow_vince-userdebug \
-    arrow_vince-eng
+    fluid_vince-user \
+    fluid_vince-userdebug \
+    fluid_vince-eng
