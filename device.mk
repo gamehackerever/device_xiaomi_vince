200fe48e751cc289cd6bf1415e3a26f76835bd81
gamehackerever <gamehackerever@gmail.com>
2021-07-24 21:11:34 +0530

    vince: Adapt and Rebrand For Vince

--- device.mk
+++ device.mk
@@ -323,7 +323,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.0.vendor \
     vendor.qti.hardware.perf@2.1.vendor \
     android.hardware.power.stats@1.0-service.mock \
-    android.hardware.power-service.rosy-libperfmgr
+    android.hardware.power-service.vince-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
--- power-libperfmgr/service.cpp
+++ power-libperfmgr/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power-service.rosy-libperfmgr"
+#define LOG_TAG "android.hardware.power-service.vince-libperfmgr"
 
 #include <thread>
 
