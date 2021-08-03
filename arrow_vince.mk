2097624eb70a3dda3f35dc78bf6755b99aeb5a98
gamehackerever <gamehackerever@gmail.com>
2021-08-03 17:13:25 +0530

    Revert "vince: Dont Set BUILD_FINGERPRINT"
    This reverts commit 2baa60776a85000c619552a08f06a1eb033eeaaf.

--- arrow_vince.mk
+++ arrow_vince.mk
@@ -44,3 +44,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vince-user 8.1.0 OPM1.171019.019 V11.0.2.0.OEGMIXM release-keys"
 
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "xiaomi/vince/vince:8.1.0/OPM1.171019.019/V11.0.2.0.OEGMIXM:user/release-keys"
