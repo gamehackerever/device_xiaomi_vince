384daef99707f06f9e55cacd79ae362108cb572a
gamehackerever <gamehackerever@gmail.com>
2021-09-03 18:42:46 +0530

    vince: Sync Bluetooth With CAF - from LA.UM.9.6.2.r1-04800-89xx.0
    Signed-off-by: gamehackerever <gamehackerever@gmail.com>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,5 +1,10 @@
 /*
- * Copyright (C) 2015 The CyanogenMod Project
+ *
+ *  Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ *  Not a Contribution, Apache license notifications and license are retained
+ *  for attribution purposes only.
+ *
+ * Copyright (C) 2012 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,19 +21,14 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-
 #define BTM_DEF_LOCAL_NAME   "Redmi 5 Plus"
-#define MAX_ACL_CONNECTIONS    16
+// Disables read remote device feature
+#define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
-
-// Wide-band speech support
-#define BTIF_HF_WBS_PREFERRED TRUE
-
-/* Disable enhanced sco commands */
-#define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
+// skips conn update at conn completion
+#define BT_CLEAN_TURN_ON_DISABLED 1
 
 /* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
 #define AVDT_NUM_SEPS 12
-
-#endif /* _BDROID_BUILDCFG_H */
+#endif
