5f30300ce813518cab6ae51e9311ced85ad1517a
gamehackerever <gamehackerever@gmail.com>
2021-10-03 11:20:48 +0530

    vince: Drop libperfmgr

--- device.mk
+++ device.mk
@@ -331,14 +331,11 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.0.vendor \
     vendor.qti.hardware.perf@2.1.vendor \
     android.hardware.power.stats@1.0-service.mock \
-    android.hardware.power-service.vince-libperfmgr
+    android.hardware.power-service-qti
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
-
-PRODUCT_SOONG_NAMESPACES += \
-    hardware/google/interfaces \
-    hardware/google/pixel
 
 # Public libraries
 PRODUCT_COPY_FILES += \
